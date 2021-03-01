d6bac5a5a4fbb5a9415dec64d10187b42b7eb6eb
Camilo Agudelo <jcagudelo12@gmail.com>
2021-02-28 23:46:07 -0500

    Register users

--- components/account/RegisterForm.jsx
+++ components/account/RegisterForm.jsx
@@ -1,17 +1,91 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Icon, Input } from "react-native-elements";
+import { size } from "lodash";
+import { useNavigation } from "@react-navigation/native";
+
+import { registerUser } from "../../utils/actions";
+import { validateEmail } from "../../utils/helpers";
 
 export default function RegisterForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValues());
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirm, setErrorConfirm] = useState("");
+
+  const navigation = useNavigation();
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+
+  const doRegisterUser = async () => {
+    if (!validateData()) {
+      return;
+    }
+
+    const result = await registerUser(formData.email, formData.password);
+    if (!result.statusResponse) {
+      setErrorEmail(result.error);
+      return;
+    }
+    navigation.navigate("restaurants");
+    console.log("llegue");
+  };
+
+  const validateData = () => {
+    setErrorConfirm("");
+    setErrorEmail("");
+    setErrorPassword("");
+    let isValid = true;
+
+    if (!validateEmail(formData.email)) {
+      setErrorEmail("Debes de ingresar un email valido");
+      isValid = false;
+    }
+
+    if (size(formData.password) < 6) {
+      setErrorPassword(
+        "Debes ingresar una contraseña de al menos 6 carácteres"
+      );
+      isValid = false;
+    }
+
+    if (size(formData.confirm) < 6) {
+      setErrorConfirm(
+        "Debes ingresar una confirmación de contraseña de al menos 6 carácteres"
+      );
+      isValid = false;
+    }
+
+    if (formData.password !== formData.confirm) {
+      setErrorConfirm("Las contraseñas no coinciden.");
+      setErrorPassword("Las contraseñas no coinciden.");
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
   return (
     <View style={styles.form}>
-      <Input containerStyle={styles.input} placeholder="Ingresa tu email" />
+      <Input
+        containerStyle={styles.input}
+        placeholder="Ingresa tu email"
+        onChange={(e) => onChange(e, "email")}
+        keyboardType="email-address"
+        errorMessage={errorEmail}
+        defaultValue={formData.email}
+      />
       <Input
         containerStyle={styles.input}
         placeholder="Ingresa tu constraseña"
         password={true}
         secureTextEntry={!showPassword}
+        onChange={(e) => onChange(e, "password")}
+        errorMessage={errorPassword}
+        defaultValue={formData.password}
         rightIcon={
           <Icon
             type="material-community"
@@ -26,6 +100,9 @@ export default function RegisterForm() {
         placeholder="Confirma tu contraseña"
         password={true}
         secureTextEntry={!showPassword}
+        onChange={(e) => onChange(e, "confirm")}
+        errorMessage={errorConfirm}
+        defaultValue={formData.confirm}
         rightIcon={
           <Icon
             type="material-community"
@@ -39,10 +116,15 @@ export default function RegisterForm() {
         title="Registrar Nuevo Usuario"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={() => doRegisterUser()}
       />
     </View>
   );
 }
+
+const defaultFormValues = () => {
+  return { email: "", password: "", confirm: "" };
+};
 
 const styles = StyleSheet.create({
   form: {
--- utils/actions.js
+++ utils/actions.js
@@ -15,3 +15,18 @@ export const isUserLogged = () => {
 export const getCurrentUser = () => {
   return firebase.auth().getCurrentUser;
 };
+
+export const registerUser = async (email, password) => {
+  const result = { statusResponse: true, error: null };
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+  } catch (error) {
+    result.statusResponse = false;
+    result.error = "Este correo ya ha sido registrado.";
+  }
+  return result;
+};
+
+export const closeSesion = () => {
+  return firebase.auth().signOut();
+};

a8980acd0fa1eb1f8271b10bae8132d8c94ecf41
Camilo Agudelo <jcagudelo12@gmail.com>
2021-03-26 17:12:43 -0500

    Add view AddRestaurant

--- screens/resturants/AddRestaurant.jsx
+++ screens/resturants/AddRestaurant.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Toast from "react-native-easy-toast";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Loading from "../../components/Loading";
 
-export default function AddRestaurant() {
+import AddRestaurantForm from "../../components/restaurants/AddRestaurantForm";
+
+export default function AddRestaurant({ navigation }) {
+  const toastRef = useRef();
+  const [loading, setLoading] = useState(false);
   return (
-    <View>
-      <Text>AddRestaurant...</Text>
-    </View>
+    <KeyboardAwareScrollView>
+      <AddRestaurantForm
+        toastRef={toastRef}
+        setLoading={setLoading}
+        navigation={navigation}
+      />
+      <Loading isVisible={loading} text="Creando restaurante..." />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+    </KeyboardAwareScrollView>
   );
 }
 
